4a52a60d0752b9050950065e39893a4d3ef07a66
siroisc <dunkhi@csu.fullerton.edu>
2019-12-10 02:36:44 -0800

    Folder swapping

--- react-firebase-authentication/src/components/Firebase/index.js
+++ react-firebase-authentication/src/components/Firebase/index.js
@@ -1,7 +1,6 @@
-import React from 'react';
-const Firebase = () => (
-  <div>
-    <h1>Firebase</h1>
-  </div>
-);
+import FirebaseContext from './context';
+import Firebase from '../components/Firebase/firebase';
+
 export default Firebase;
+
+export { FirebaseContext };
