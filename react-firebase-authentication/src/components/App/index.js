e2670e040096407bda6ae1e44bb60c5ef9bb1d96
siroisc <dunkhi@csu.fullerton.edu>
2019-12-10 16:17:45 -0800

    Sign in and sign up page with firebase

--- react-firebase-authentication/src/components/App/index.js
+++ react-firebase-authentication/src/components/App/index.js
@@ -21,16 +21,18 @@ const App = () => (
       <Navigation />
 
       <hr />
-
+      
       <Route exact path={ROUTES.LANDING} component={LandingPage} />
       <Route exact path={ROUTES.SIGN_UP} component={SignUpPage} />
       <Route exact path={ROUTES.SIGN_IN} component={SignInPage} />
-      <Route exact path={ROUTES.PASSWORD_FORGET} component={PasswordForgetPage} />
+      <Route 
+        exact 
+        path={ROUTES.PASSWORD_FORGET} 
+        component={PasswordForgetPage} />
       <Route exact path={ROUTES.HOME} component={HomePage} />
       <Route exact path={ROUTES.ACCOUNT} component={AccountPage} />
       <Route exact path={ROUTES.ADMIN} component={AdminPage} />
     </div>
-    
   </Router>
 );
 export default App;
